f340ffec7bdb75c4617d84c70a42c59583c01baa
Thomas Cherickal <thomascherickal@gmail.com>
2022-02-10 22:41:20 +0530

    Refactored!

---
+++ .vscode/DecryptRSA.py
@@ -0,0 +1,84 @@
+
+import numpy as np
+from math import sqrt,log,gcd
+import random
+from random import randint
+import sys
+
+class Decrypt(object):
+    
+    def mod_inverse(self, a, m):
+        for x in range(1, m):
+            if (a * x) % m == 1:
+                return x
+        return -1
+
+    def isprime(self, n):
+        if n < 2:
+            return False
+        elif n == 2:
+            return True
+        else:
+            for i in range(1, int(sqrt(n)) + 1):
+                if n % i == 0:
+                    return False
+        return True
+
+    def generate_keypair(self, keysize):
+        p = randint(1, 1000)
+        q = randint(1, 1000)
+        nMin = 1 << (keysize - 1)
+        nMax = (1 << keysize) - 1
+        primes = [2]
+        start = 1 << (keysize // 2 - 1)
+        stop = 1 << (keysize // 2 + 1)
+        if start >= stop:
+            return []
+        for i in range(3, stop + 1, 2):
+            for p in primes:
+                if i % p == 0:
+                    break
+            else:
+                primes.append(i)
+        while (primes and primes[0] < start):
+            del primes[0]
+        # Select two random prime numbers p and q
+        while primes:
+            p = random.choice(primes)
+            primes.remove(p)
+            q_values = [q for q in primes if nMin <= p * q <= nMax]
+            if q_values:
+                q = random.choice(q_values)
+                break
+        # Calculate n
+        n = p * q
+        # Calculate phi
+        phi = (p - 1) * (q - 1)
+        # Select e
+        e = random.randrange(1, phi)
+        g = gcd(e, phi)
+        # Calculate d
+        while True:
+            e = random.randrange(1, phi)
+            g = gcd(e, phi)
+            d = self.mod_inverse(e, phi)
+            if g == 1 and e != d:
+                break
+
+        return ((e, n), (d, n))
+
+
+
+    def decrypt(self, ciphertext, package):
+        d, n = package
+        plaintext = [chr(pow(c, d, n)) for c in ciphertext]
+        return (''.join(plaintext))
+
+    def main(self, argv):
+        return self.decrypt(argv[1])
+
+    if __name__ == '__DecryptRSA__':
+        main(sys.argv)
+        
+        
+
